c63ff7585433134ff58cd075b078407e5a49a6cd
root <root@localhost.localdomain>
2026-08-04 20:45:42 +0000

    fix: minSdk 24 untuk flutter_tts kompatibilitas, rename package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.teleman70001.hermestalk_project"
+    namespace = "com.teleman70001.hermestalk"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,10 +21,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.teleman70001.hermestalk_project"
+        applicationId = "com.teleman70001.hermestalk"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
